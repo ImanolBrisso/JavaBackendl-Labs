c7d949f4fd52f937957163833abea4c8f4783a76
ImanolBrisso <imanolbrissolese22@gmail.com>
2024-10-26 21:11:17 -0300

    cambios y agregada base de datos

--- demoSpring/src/main/java/com/ejemplo/demo/controller/PersonaController.java
+++ demoSpring/src/main/java/com/ejemplo/demo/controller/PersonaController.java
@@ -9,31 +9,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-// Este es un controlador de Spring MVC
+// este es un controlador de Spring MVC
 @Controller
 public class PersonaController {
 
     @Autowired
     private PersonaRepository personaRepository;
 
-    // se establecen los metodos manejadores de get y post - se recuperan y envian datos
+    // metodos manejadores de get y post
 
-    // es el manejador de la raiz
+    // este el manejador de la raiz
     @GetMapping("/")
     public String verPaginaInicio(Model modelo) {
         modelo.addAttribute("personas", personaRepository.findAll());
         return "index";
     }
 
-
-    // mostrar un formulario para crear una persona nueva - Retorna
+    // mostrar un formulario para crear una persona nueva
+    // Model permite pasar datos del controlador a la vista
+    // la solicitud get se pasa a `http://localhost:8080/nueva`
     @GetMapping("/nueva")
     public String mostrarFormularioPersona(Model modelo) {
         modelo.addAttribute("persona", new Persona());
         return "nueva_persona";
     }
 
-    // maneja el envio del formulario y guarda a la persona - Redirecciona a la raiz del manejador getmapping
+    // maneja el envio del formulario y guarda la persona
     @PostMapping("/guardar")
     public String guardarPersona(@ModelAttribute Persona persona) {
         personaRepository.save(persona);
--- demoSpring/src/main/java/com/ejemplo/demo/repository/PersonaRepository.java
+++ demoSpring/src/main/java/com/ejemplo/demo/repository/PersonaRepository.java
@@ -4,6 +4,8 @@ import com.ejemplo.demo.model.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// operaciones CRUD: create read update delete
+
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 }
